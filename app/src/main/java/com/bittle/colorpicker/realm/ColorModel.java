bdc81e8126c9ebd82b4382bddb6ea6b267272d31
oscar0812 <oscar0812torres@gmail.com>
2018-05-21 18:12:55 -0500

    Realm helper class and model

---
+++ app/src/main/java/com/bittle/colorpicker/realm/ColorModel.java
@@ -0,0 +1,65 @@
+package com.bittle.colorpicker.realm;
+
+import com.bittle.colorpicker.utils.ColorUtil;
+
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+import io.realm.annotations.Required;
+
+/**
+ * Scribbled by oscartorres on 5/18/18.
+ */
+public class ColorModel extends RealmObject {
+    private long timestamp;
+    @Required
+    @PrimaryKey
+    private String hex;
+    private String name = null;
+
+    public ColorModel(){}
+
+    public ColorModel(String name, int r, int g, int b){
+        this.name = name;
+        this.hex = ColorUtil.rgbToHex(r, g, b);
+    }
+
+    public ColorModel(String hex) {
+        this.hex = hex;
+        this.timestamp = System.currentTimeMillis() / 1000;
+    }
+
+    public ColorModel updateTime() {
+        this.timestamp = System.currentTimeMillis() / 1000;
+        return this;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public String getHex() {
+        return hex;
+    }
+
+    // methods to display on list
+    public int getColor() {
+        return ColorUtil.getInstance().hexToColor(hex);
+    }
+
+    public String getName() {
+        if(name == null){
+            String new_name = ColorUtil.getInstance().getClosestColor(this);
+            try {
+                name = new_name;
+            }catch (Exception e){
+                return new_name;
+            }
+        }
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return o instanceof ColorModel && hex.equals(((ColorModel) o).getHex());
+    }
+}
---
+++ app/src/main/java/com/bittle/colorpicker/realm/DBRealm.java
@@ -0,0 +1,78 @@
+package com.bittle.colorpicker.realm;
+
+import android.content.Context;
+
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+import io.realm.RealmResults;
+import io.realm.Sort;
+
+/**
+ * Scribbled by oscartorres on 5/20/18.
+ */
+
+// helper for realm db
+public class DBRealm {
+
+    private static DBRealm instance = null;
+
+    public static DBRealm getInstance(Context context) {
+        if (instance == null)
+            instance = new DBRealm(context);
+        return instance;
+    }
+
+    private Realm realm;
+
+    private DBRealm(Context context) {
+        // initialize realm
+        Realm.init(context);
+        RealmConfiguration config = new RealmConfiguration.Builder().name("colorpicker.realm").build();
+        Realm.setDefaultConfiguration(config);
+        start();
+    }
+
+    // insert color into db
+    public void insert(String hex) {
+        try {
+            realm.beginTransaction();
+            ColorModel c = realm.where(ColorModel.class).
+                    equalTo("hex", hex).findFirst();
+            if (c == null) {
+                realm.insert(new ColorModel(hex));
+            } else {
+                realm.insertOrUpdate(c.updateTime());
+            }
+            realm.commitTransaction();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public RealmResults<ColorModel> findAll() {
+        RealmResults<ColorModel> results =
+                realm.where(ColorModel.class).sort("timestamp", Sort.DESCENDING).findAll();
+        //realm.close();
+        return results;
+    }
+
+    // clear db
+    public void clearAll() {
+        try {
+            realm.beginTransaction();
+            realm.where(ColorModel.class).findAll().deleteAllFromRealm();
+            realm.commitTransaction();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            //realm.close();
+        }
+    }
+
+    public void start() {
+        this.realm = Realm.getDefaultInstance(); // opens "colorpicker.realm"
+    }
+    public void close(){
+        realm.close();
+    }
+}
