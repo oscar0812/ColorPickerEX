d9925be0cab192e16cb45942245476ec0a5b473a
oscar0812 <oscar0812torres@gmail.com>
2018-05-26 20:01:56 -0500

    put toast in a try catch

--- app/src/main/java/com/bittle/colorpicker/utils/Toaster.java
+++ app/src/main/java/com/bittle/colorpicker/utils/Toaster.java
@@ -13,11 +13,14 @@ public class Toaster {
     private static Handler mUiHandler = new Handler(Looper.getMainLooper());
 
     public static void toast(final String line, final Context context) {
-        mUiHandler.post(new Runnable() {
-            @Override
-            public void run() {
-                Toast.makeText(context, line, Toast.LENGTH_LONG).show();
-            }
-        });
+        try {
+            mUiHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(context, line, Toast.LENGTH_LONG).show();
+                }
+            });
+        } catch (Exception ignored) {
+        }
     }
 }
