904e16305cf220ee5f13f458a4b9ebb72a4cb855
oscar0812 <oscar0812torres@gmail.com>
2018-05-31 23:55:49 -0500

    Convert Activity and layout update

--- app/src/main/java/com/bittle/colorpicker/ConvertMainActivity.java
+++ app/src/main/java/com/bittle/colorpicker/ConvertMainActivity.java
@@ -1,27 +1,18 @@
 package com.bittle.colorpicker;
 
-import android.content.Context;
-import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.Typeface;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
 
-import com.bittle.colorpicker.dialogs.ColorInfoDialog;
-
-import com.bittle.colorpicker.utils.ColorUtil;
-import com.bittle.colorpicker.utils.StringUtil;
-
-public class ConvertMainActivity extends AppCompatActivity {
+public class ConvertMainActivity extends BaseDrawerActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.convert_layout);
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
 
     }
 }
